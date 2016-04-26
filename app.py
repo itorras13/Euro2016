319804672ce25c8e2121b886f2bc982089dc4f72
itorras13 <itorras13@gmail.com>
2016-04-26 13:55:06 -0400

    started making index page

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import render_template
+from flask import redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return render_template('index.html')
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return redirect(url_for('index'))
 
 if __name__ == "__main__":
     app.run()
