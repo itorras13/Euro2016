d6d9580d9a3fb1e04364283352f5db06f5865511
itorras13 <itorras13@gmail.com>
2016-05-01 20:15:54 -0400

    completely changed form to calculate group standings by itself

--- static/js/selects.js
+++ static/js/selects.js
@@ -1,33 +1,8 @@
 $(document).ready(function(){
-    var quarterFinalists = [];
-    var semiFinalists = [];
-    var finalists = [];
-    var champs = [];
+    text = "Choose a Team";
     var index = 0;
-  $('[data-toggle="tooltip"]').tooltip();
+    var finalists = [];
   $('.quarter_select').on('change', function(event ) {
-    var prevValue = $(this).data('previous');
-    index = semiFinalists.indexOf(prevValue);
-    if (index > -1) {
-        semiFinalists.splice(index, 1);
-    }
-    $('.quarter_select').not(this).find('option[value="'+prevValue+'"]').show();
-    var value = $(this).val();
-    semiFinalists.push(value);
-    semiFinalists.sort();
-    $(this).data('previous',value);
-    $('.quarter_select').not(this).find('option[value="'+value+'"]').hide();
-    $('.semi_select')
-        .find('option')
-        .remove()
-        .end()
-        .append('<option disabled selected value style="display: none"></option>')
-    ;
-    for (var i = 0; i < semiFinalists.length; ++i) {
-        $('.semi_select').append($("<option></option>")
-                    .attr("value",semiFinalists[i])
-                    .text(semiFinalists[i])); 
-    }
     var q1 = $("#q1").children("option").filter(":selected").text();
     var q2 = $("#q2").children("option").filter(":selected").text();
     var q3 = $("#q3").children("option").filter(":selected").text();
@@ -36,10 +11,22 @@ $(document).ready(function(){
     var q6 = $("#q6").children("option").filter(":selected").text();
     var q7 = $("#q7").children("option").filter(":selected").text();
     var q8 = $("#q8").children("option").filter(":selected").text();
-    $(".s1").attr('data-original-title', q1 + " v " + q2 ).tooltip('setContent');
-    $(".s2").attr('data-original-title', q3 + " v " + q4 ).tooltip('setContent');
-    $(".s3").attr('data-original-title', q5 + " v " + q6 ).tooltip('setContent');
-    $(".s4").attr('data-original-title', q7 + " v " + q8 ).tooltip('setContent');
+    if (q1!=text&&q2!=text&&q3!=text&&q4!=text&&q5!=text&&q6!=text&&q7!=text&&q8!=text) {
+        $('.semi_select')
+            .find('option')
+            .remove()
+            .end()
+            .append('<option disabled selected value style="display: none">Choose a Team</option>')
+        ;
+        $('#s1').append($("<option></option>").attr("value",q1).text(q1));
+        $('#s1').append($("<option></option>").attr("value",q2).text(q2));
+        $('#s2').append($("<option></option>").attr("value",q3).text(q3));
+        $('#s2').append($("<option></option>").attr("value",q4).text(q4));
+        $('#s3').append($("<option></option>").attr("value",q5).text(q5));
+        $('#s3').append($("<option></option>").attr("value",q6).text(q6));
+        $('#s4').append($("<option></option>").attr("value",q7).text(q7));
+        $('#s4').append($("<option></option>").attr("value",q8).text(q8));
+    }
   });
   $('.semi_select').on('change', function(event ) {
     var prevValue = $(this).data('previous');
@@ -47,103 +34,49 @@ $(document).ready(function(){
     if (index > -1) {
         finalists.splice(index, 1);
     }
-    $('.semi_select').not(this).find('option[value="'+prevValue+'"]').show();
     var value = $(this).val();
     finalists.push(value);
     finalists.sort();
     $(this).data('previous',value);
-    $('.semi_select').not(this).find('option[value="'+value+'"]').hide();
-    $('.fin_select')
-        .find('option')
-        .remove()
-        .end()
-        .append('<option disabled selected value style="display: none"></option>')
-    ;
-    for (var i = 0; i < finalists.length; ++i) {
-        $('.fin_select').append($("<option></option>")
-                    .attr("value",finalists[i])
-                    .text(finalists[i])); 
-    }
     var s1 = $("#s1").children("option").filter(":selected").text();
     var s2 = $("#s2").children("option").filter(":selected").text();
     var s3 = $("#s3").children("option").filter(":selected").text();
     var s4 = $("#s4").children("option").filter(":selected").text();
-    $(".fin1").attr('data-original-title', s1 + " v " + s2 ).tooltip('setContent');
-    $(".fin2").attr('data-original-title', s3 + " v " + s4 ).tooltip('setContent');
+    if (s1!=text&&s2!=text&&s3!=text&&s4!=text) {
+        $('.fin_select')
+            .find('option')
+            .remove()
+            .end()
+            .append('<option disabled selected value style="display: none">Choose a Team</option>')
+        ;
+        for (var i = 0; i < finalists.length; ++i) {
+            $('#third_place').append($("<option></option>")
+                        .attr("value",finalists[i])
+                        .text(finalists[i])); 
+        }
+        $('#fin1').append($("<option></option>").attr("value",s1).text(s1));
+        $('#fin1').append($("<option></option>").attr("value",s2).text(s2));
+        $('#fin2').append($("<option></option>").attr("value",s3).text(s3));
+        $('#fin2').append($("<option></option>").attr("value",s4).text(s4));
+    }
   });
   $('.fin_select').on('change', function(event ) {
     var prevValue = $(this).data('previous');
-    index = champs.indexOf(prevValue);
-    if (index > -1) {
-        champs.splice(index, 1);
-    }
-    $('.fin_select').not(this).find('option[value="'+prevValue+'"]').show();
+    $('#third_place').not(this).find('option[value="'+prevValue+'"]').show();
     var value = $(this).val();
-    champs.push(value);
-    champs.sort();
     $(this).data('previous',value);
-    $('.fin_select').not(this).find('option[value="'+value+'"]').hide();
-    $('.champs_select')
-        .find('option')
-        .remove()
-        .end()
-        .append('<option disabled selected value style="display: none"></option>')
-    ;
-    for (var i = 0; i < champs.length; ++i) {
-        $('.champs_select').append($("<option></option>")
-                    .attr("value",champs[i])
-                    .text(champs[i])); 
-    }
+    $('#third_place').not(this).find('option[value="'+value+'"]').hide();
     var fin1 = $("#fin1").children("option").filter(":selected").text();
     var fin2 = $("#fin2").children("option").filter(":selected").text();
-    $(".champion").attr('data-original-title', fin1 + " v " + fin2 ).tooltip('setContent');
-  });
-  $('.group_select').on('change', function(event ) {
-    var prevValue = $(this).data('previous');
-    index = quarterFinalists.indexOf(prevValue);
-    if (index > -1) {
-        quarterFinalists.splice(index, 1);
+    if (fin1!=text&&fin2!=text) {  
+        $('.champs_select')
+            .find('option')
+            .remove()
+            .end()
+            .append('<option disabled selected value style="display: none">Choose a Team</option>')
+        ;
+        $('#champion').append($("<option></option>").attr("value",fin1).text(fin1));
+        $('#champion').append($("<option></option>").attr("value",fin2).text(fin2));
     }
-    $('.group_select').not(this).find('option[value="'+prevValue+'"]').show();
-    var value = $(this).val();
-    quarterFinalists.push(value);
-    quarterFinalists.sort();
-    $(this).data('previous',value);
-    $('.group_select').not(this).find('option[value="'+value+'"]').hide();
-    $('.quarter_select')
-        .find('option')
-        .remove()
-        .end()
-        .append('<option disabled selected value style="display: none"></option>')
-    ;
-    for (var i = 0; i < quarterFinalists.length; ++i) {
-        $('.quarter_select').append($("<option></option>")
-                    .attr("value",quarterFinalists[i])
-                    .text(quarterFinalists[i])); 
-    }
-    var a1 = $("#a1").children("option").filter(":selected").text();
-    var a2 = $("#a2").children("option").filter(":selected").text();
-    var b1 = $("#b1").children("option").filter(":selected").text();
-    var b2 = $("#b2").children("option").filter(":selected").text();
-    var c1 = $("#c1").children("option").filter(":selected").text();
-    var c2 = $("#c2").children("option").filter(":selected").text();
-    var d1 = $("#d1").children("option").filter(":selected").text();
-    var d2 = $("#d2").children("option").filter(":selected").text();
-    var e1 = $("#e1").children("option").filter(":selected").text();
-    var e2 = $("#e2").children("option").filter(":selected").text();
-    var f1 = $("#f1").children("option").filter(":selected").text();
-    var f2 = $("#f2").children("option").filter(":selected").text();
-    var third1 = $("#third1").children("option").filter(":selected").text();
-    var third2 = $("#third2").children("option").filter(":selected").text();
-    var third3 = $("#third3").children("option").filter(":selected").text();
-    var third4 = $("#third4").children("option").filter(":selected").text();    
-    $(".q1").attr('data-original-title', a2 + " v " + c2 ).tooltip('setContent');
-    $(".q2").attr('data-original-title', d1 + " v " + third1 ).tooltip('setContent');
-    $(".q3").attr('data-original-title', b1 + " v " + third2 ).tooltip('setContent');
-    $(".q4").attr('data-original-title', f1 + " v " + e2 ).tooltip('setContent');
-    $(".q5").attr('data-original-title', c1 + " v " + third3 ).tooltip('setContent');
-    $(".q6").attr('data-original-title', e1 + " v " + d2 ).tooltip('setContent');
-    $(".q7").attr('data-original-title', a1 + " v " + third4 ).tooltip('setContent');
-    $(".q8").attr('data-original-title', b2 + " v " + f2 ).tooltip('setContent');
   });
 });
