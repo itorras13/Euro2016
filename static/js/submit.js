d04a5508a45ae161247bf7cafd102ba8fffc8287
itorras13 <itorras13@gmail.com>
2016-05-16 11:56:04 -0400

    changed var name

--- static/js/submit.js
+++ static/js/submit.js
@@ -9,29 +9,29 @@ $(document).ready(function(){
 	        	var inputs = ["q1", "q2", "q3", "q4", "q5", "q6", "q7", "q8", "semi1",
 	        		"semi2", "semi3", "semi4", "fin1", "fin2", "champion", "third_place", "top_scorer"]
 	            for (var i = 0; i < inputs.length; ++i) {
-	            	text = "Your personal information has not been entered"
+	            	error_text = "Your personal information has not been entered"
 					current = 'select[name="' + inputs[i] + '"]';
 					current_value = $(current).val();
 					if (current_value == null) {
 						string_array = inputs[i].split("");
 						if (string_array[0] == "q"){
-							text = "Quarter Finalist " + string_array[1] + " is not chosen!";
+							error_text = "Quarter Finalist " + string_array[1] + " is not chosen!";
 						} else if (string_array[0] == "s"){
-							text = "Semi Finalist " + string_array[4] + " is not chosen!";
+							error_text = "Semi Finalist " + string_array[4] + " is not chosen!";
 						} else if (string_array[0] == "f"){
-							text = "Finalist " + string_array[3] + " is not chosen!";
+							error_text = "Finalist " + string_array[3] + " is not chosen!";
 						} else if (string_array[0] == "c"){
-							text = "The champion is not chosen!";
+							error_text = "The champion is not chosen!";
 						} else if (inputs[i] == "third_place"){
-							text = "Third Place is not chosen!";
+							error_text = "Third Place is not chosen!";
 						} else if (inputs[i] == "top_scorer"){
-							text = "The top scorer is not chosen!";
+							error_text = "The top scorer is not chosen!";
 						}
 						break;
 					}
 				}
 				$('#modalHead').html('<i class="glyphicon glyphicon-thumbs-down"></i>  Not all the teams have been chosen!')
-				$('#modalParagraph').html("<b>" + text + "</b>")
+				$('#modalParagraph').html("<b>" + error_text + "</b>")
 				$('#notFinishedModal').modal('show');
 	        }
 	    }, false);
